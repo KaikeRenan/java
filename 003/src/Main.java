6b9149b6ca5d2cbaccd529d5dd663d77bad38be7
kaike-renan <kaikealmeida98@outlook.com>
2024-06-09 20:46:34 -0300

    aula 3

---
+++ 003/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        float divi = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(divi);
+    }
+}
