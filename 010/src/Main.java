adca0dcebeeca5b4add4b89a4762127af4a05afd
kaike-renan <kaikealmeida98@outlook.com>
2024-06-11 15:15:58 -0300

    aula 10

---
+++ 010/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+
+        byte variavel_byte = 120;
+        System.out.println(variavel_byte);
+
+        int variavel_int = 2135161;
+        System.out.println(variavel_int);
+    }
+}
